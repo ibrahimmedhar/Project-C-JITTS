86c8239303a346cd6f970dd5ee4dcaf585489ee7
Tim Feskens <0967290@hr.nl>
2020-10-05 11:19:53 +0200

    Transfered project to C# Forms

---
+++ ProjectC-JITTS/ProjectC-JITTS/Database/Connecter.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ProjectC_JITTS.Database
+{
+    class Connecter
+    {
+        protected MySqlConnection Connection;
+
+        protected Connecter()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            string dbstring = "server=localhost;user=root;pwd=admin;database=projectc";
+            Connection = new MySqlConnection(dbstring);
+        }
+    }
+
+
+}
---
+++ ProjectC-JITTS/ProjectC-JITTS/Home.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ProjectC_JITTS.Database;
+
+namespace ProjectC_JITTS
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+
+			GetData GD = new GetData();
+			int x = 20;
+
+			foreach (Tuple<string, string, string, string> room in GD.ShowRooms())
+			{
+				Label roomLabel = new Label();
+				roomLabel.Width = 150;
+				roomLabel.Height = 200;
+				roomLabel.BorderStyle = BorderStyle.FixedSingle;
+				roomLabel.Cursor = Cursors.Hand;
+				roomLabel.Text = "Room no: " + room.Item1;
+				roomLabel.Text += "\nWorkplaces : " + room.Item2;
+				roomLabel.Text += "\nWorkplaces Available " + room.Item3;
+				roomLabel.Text += "\nLocation: " + room.Item4;
+
+				roomLabel.Location = new Point(0 + x, 120);
+				roomLabel.AutoSize = true;
+
+				this.Controls.Add(roomLabel);
+
+				x += 200;
+			}
+		}
+	}
+}
